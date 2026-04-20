1391bd36f83fb76cc115a9c3f006c072a0631d5c
Julio7-7-7 <julio.toledo2030@gmail.com>
2026-04-20 10:57:41 -0400

    fetch: engranando todo en la rama kde

--- postgrado/models/tipo_programa.py
+++ postgrado/models/tipo_programa.py
@@ -4,5 +4,5 @@ from database import Base
 class TipoPrograma(Base):
     __tablename__ = "tipo_programa"
 
-    id_tipo_programa = Column(Integer, primary_key=True, index=True)
+    id_tipo_programa = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nombre = Column(String, nullable=False)
